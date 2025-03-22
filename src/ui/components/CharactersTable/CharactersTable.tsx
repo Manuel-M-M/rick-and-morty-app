10d1c3a0ac2e1b7fcae12b8824f87b5b2ce5e453
Manuel-M-M <mmoragamolina@gmail.com>
2025-03-22 14:44:07 +0100

    feat: render character row with name, gender, status, species, location and episode count

---
+++ src/ui/components/CharactersTable/CharactersTable.tsx
@@ -0,0 +1,19 @@
+const CharactersTable = () => {
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Gender</th>
+          <th>Status</th>
+          <th>Species</th>
+          <th>Last Known Location</th>
+          <th>Episodes</th>
+        </tr>
+      </thead>
+      <tbody />
+    </table>
+  );
+};
+
+export default CharactersTable;
