cd00e370efe4d61c291295d102aec500c56428f0
Manuel-M-M <mmoragamolina@gmail.com>
2025-03-23 12:31:40 +0100

    feat: implement character search via controlled input and API call

--- src/ui/pages/CharactersListPage.tsx
+++ src/ui/pages/CharactersListPage.tsx
@@ -1,16 +1,25 @@
 import { useEffect, useState } from "react";
 import { Character } from "../../domain/models/Character";
-import { fetchCharacters } from "../../infrastructure/api/rickAndMortyApi";
+import { fetchCharactersByName } from "../../infrastructure/api/rickAndMortyApi";
 import CharactersTable from "../components/CharactersTable/CharactersTable";
+import CharacterSearchInput from "../components/CharacterSearchInput/CharacterSearchInput";
 
 const CharactersListPage = () => {
   const [characters, setCharacters] = useState<Character[]>([]);
+  const [search, setSearch] = useState("");
 
   useEffect(() => {
-    fetchCharacters().then(setCharacters);
-  }, []);
+    fetchCharactersByName(search)
+      .then(setCharacters)
+      .catch(() => setCharacters([]));
+  }, [search]);
 
-  return <CharactersTable characters={characters} />;
+  return (
+    <>
+      <CharacterSearchInput value={search} onChange={setSearch} />
+      <CharactersTable characters={characters} />
+    </>
+  );
 };
 
 export default CharactersListPage;
